ea39bf03c5b6478cb75dbad9f10b97a78d9b4eca
Kübra Emektar <124355274+emektarkubra@users.noreply.github.com>
2024-02-21 17:27:29 +0300

    added SessionProvider and SessionGuard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Providers } from "./Providers";
+import SessionGuard from "@/components/SessionGuard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <SessionGuard>
+            {children}
+          </SessionGuard>
+        </Providers>
+      </body>
     </html>
   );
 }
